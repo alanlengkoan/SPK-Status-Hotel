69d59b719066ae8bcba11094fc00529d98024823
alanlengkoan <alansaputralengkoan@gmail.com>
2022-08-28 12:56:07 +0800

    add port

--- database/koneksi.php
+++ database/koneksi.php
@@ -1,15 +1,14 @@
 <?php
 
-$host = 'localhost';
+$host = '127.0.0.1';
 $user = 'my_root';
 $pass = 'my_pass';
 $dbnm = 'spk_hotel';
+$port = '3307';
 
 // untuk koneksi ke database
-$koneksi = mysqli_connect($host, $user, $pass, $dbnm);
+$koneksi = mysqli_connect($host, $user, $pass, $dbnm, $port);
 
 if (!$koneksi) {
     die('Gagal' . mysqli_connect_errno());
 }
-
-?>
